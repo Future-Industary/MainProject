c36c5641af8f01898602f5d4479bd9d662380cea
qanbary <qnbrymbyna24@gmail.com>
2025-06-03 00:30:22 +0330

    create a controller,API,and model for the users table

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/users/store',[UsersController::class,'store']);
